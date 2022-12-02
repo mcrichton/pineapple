4ba765e59805391ab68b56d957e3c7c1ab5a10b3
M. Crichton <email@crichton.ma>
2022-12-02 11:18:05 +0000

    feat: support Py3.11 docs

--- pineapple.user.js
+++ pineapple.user.js
@@ -2,45 +2,50 @@
 // @name         Pineapple-Devloader
 // @license      MIT (https://opensource.org/licenses/MIT)
 // @version      1.0.0
-// @match        https://docs.python.org/3/*
+// @match        https://docs.python.org/*/*
 // @run-at       document-end
 // ==/UserScript==
 
 const CSS = [
-    {
-        s: "body",
-        r: `font-size: 14px;`
-    },
-    {
-        s: "div.body",
-        r: `max-width: initial;`
-    },
-    {
-        s: ".sphinxsidebarwrapper",
-        r: `position: relative;`
-    },
-    {
-        s: "pre",
-        r: `background: #f5f5f5;`
-    },
-    {
-        s: ".versionmodified",
-        r: `color: red; font-weight: bold; font-size: 110%;`,
-    },
+	{
+		s: "body",
+		r: `font-size: 14px;`
+	},
+	{
+		s: "div.body",
+		r: `max-width: initial;`
+	},
+	{
+		s: ".sphinxsidebarwrapper",
+		r: `position: relative;`
+	},
+	{
+		s: "pre",
+		r: `background: #f5f5f5;`
+	},
+	{
+		s: ".versionmodified",
+		r: `color: red; font-weight: bold; font-size: 110%;`
+	},
+	{
+		s: ".sig-object",
+		r: `border-top: 1px solid #3333;`
+	},
 ];
 
 (() => {
-    const $hr = $(`hr.docutils`);
+	const $hr = $(`hr.docutils`);
 
-    const docData = $(`dt code.descname`).map((i,e) => {
-        const $e = $(e).parent();
-        const className = $e.find(".descclassname").text();
-        const methodName = $e.find(".descname").text();
-        const url = $e.find("a.headerlink").attr("href");
-        return {className, methodName, url}
-    }).get();
+	// `Pre 3.11`, `3.11+`
+	const docData = $(`dt code.descname, dt span.descname`).map((i,e) => {
+		const $e = $(e).parent();
+		const className = $e.find(".descclassname").text();
+		const methodName = $e.find(".descname").text();
+		const url = $e.find("a.headerlink").attr("href");
+		return {className, methodName, url}
+	}).get();
 
-    $hr.after(`<div style="width: 100%; display: flex; flex-direction: column;">
+	$hr.after(`<div style="width: 100%; display: flex; flex-direction: column;">
         <h4>Module Contents</h4>
         <div style="column-count: 4">
             ${docData.map(dd => `<div style="width: 100%; margin-bottom: 1px; border-bottom: 1px solid #ccc; padding: 1px;">
@@ -51,30 +56,30 @@ const CSS = [
 })();
 
 window.addEventListener("load", () => {
-    (function injectCss () {
-        function addCss (sheet, selector, rules){
-            const index = sheet.cssRules.length;
-            try {
-                if ("insertRule" in sheet) {
-                    sheet.insertRule(selector + "{" + rules + "}", index);
-                } else if ("addRule" in sheet) {
-                    sheet.addRule(selector, rules, index);
-                }
-            } catch (e) {
-                if ((!selector && selector.startsWith("-webkit-"))) {
-                    console.error(e);
-                    console.error(`Selector was "${selector}"; rules were "${rules}"`);
-                }
-            }
-        }
+	(function injectCss () {
+		function addCss (sheet, selector, rules){
+			const index = sheet.cssRules.length;
+			try {
+				if ("insertRule" in sheet) {
+					sheet.insertRule(selector + "{" + rules + "}", index);
+				} else if ("addRule" in sheet) {
+					sheet.addRule(selector, rules, index);
+				}
+			} catch (e) {
+				if ((!selector && selector.startsWith("-webkit-"))) {
+					console.error(e);
+					console.error(`Selector was "${selector}"; rules were "${rules}"`);
+				}
+			}
+		}
 
-        function addAllCss () {
-            const targetSheet =  [...window.document.styleSheets]
-                .reverse()[0];
+		function addAllCss () {
+			const targetSheet =  [...window.document.styleSheets]
+				.reverse()[0];
 
-            CSS.forEach(r => addCss(targetSheet, r.s, r.r));
-        }
+			CSS.forEach(r => addCss(targetSheet, r.s, r.r));
+		}
 
-        addAllCss();
-    })();
+		addAllCss();
+	})();
 });
